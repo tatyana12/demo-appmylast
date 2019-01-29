5351868a57dd388b3aab19a8aedaffb92ad7401c
tatyana12 <tatyana_pol@yahoo.com>
2019-01-29 10:26:32 -0800

    M

--- src/App.js
+++ src/App.js
@@ -41,10 +41,10 @@ const AppRouter = () => (
 
 
      <center><div class="shadowbox"><body id="myBody">
-        <a href="#myBody">Make it Blue</a></body></div></center>
+        <a href="#myBody">First it Blue</a></body></div></center>
 
       <center><div class="shadowbox"><body1 id="myBody1">
-        <a href="#myBody1">Or Green</a></body1></div></center>
+        <a href="#myBody1">or Green</a></body1></div></center>
 
 
 
